1f730b2375a92c773a7082c909b077e3f1034fc4
Bùi Hữu Lộc <feature451@gmail.com>
2022-07-02 03:16:42 +0700

    reformat view

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -5,11 +5,11 @@
     @extends('layout-landing-site.master')
     @section('content')
     <div class="wrapper">
-        @include('landing_page.header')
-        @include('landing_page.about')
-        @include('landing_page.classes')
-        @include('landing_page.testimonies')
-        @include('landing_page.contact')
+        @include('landing.header')
+        @include('landing.about')
+        @include('landing.classes')
+        @include('landing.testimonies')
+        @include('landing.contact')
         @include('layout-landing-site.footer')
     </div>
     @endsection
