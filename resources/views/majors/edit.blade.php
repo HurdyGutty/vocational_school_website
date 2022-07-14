32b6a6f3f30a14a405c2e0e834802a33988651f8
HurdyGutty <htnhat1996@gmail.com>
2022-07-15 01:33:20 +0700

    Fix UI

--- resources/views/majors/edit.blade.php
+++ resources/views/majors/edit.blade.php
@@ -4,49 +4,49 @@
 @endpush
 @section('content')
 <div class="card-body">
-    <h2 class="header-title">Sửa thông tin ngành</h4>
-        @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+    <h2 class="header-title">Sửa thông tin ngành</h2>
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+    <form method="POST" action="{{ route('admin.major.update') }}">
+        @method('PUT')
+        @csrf
+        <input type="hidden" id="major_id" name="id" value="{{ $major->id }}">
+        <div class="form-group">
+            <label for="major_name" class="col-form-label">Tên ngành</label>
+            <input type="text" class="form-control" id="major_name" name="name" value="{{ $major->name }}">
         </div>
-        @endif
-        <form method="POST" action="{{ route('admin.major.update') }}">
-            @method('PUT')
-            @csrf
-            <input type="hidden" id="major_id" name="id" value="{{ $major->id }}">
-            <div class="form-group">
-                <label for="major_name" class="col-form-label">Tên ngành</label>
-                <input type="text" class="form-control" id="major_name" name="name" value="{{ $major->name }}">
-            </div>
-            <div class="form-group">
-                <label for="subject_tags" class="col-form-label">Tên môn:</label>
-                <input type="hidden" class="tagsinput" data-role="tagsinput some_text" name="subjects"
-                    data-color="danger" id="subject_tags" />
-            </div>
+        <div class="form-group">
+            <label for="subject_tags" class="col-form-label">Tên môn:</label>
+            <input type="hidden" class="tagsinput" data-role="tagsinput some_text" name="subjects" data-color="danger"
+                id="subject_tags" />
+        </div>
 
-            <div class="form-group">
-                <label for="add_subject">Thêm môn</label>
-                <select id="add_subject" class="form-control">
-                    <option value="" disabled selected>Hãy chọn môn</option>
-                    @foreach ($subjects as $subject)
-                    @if (in_array($subject->id,array_keys($subject_arr)))
-                    <option value="{{ $subject->id }}" hidden>{{ $subject->name }}</option>
-                    @continue
-                    @endif
-                    <option value="{{ $subject->id }}">{{ $subject->name }}</option>
-                    @endforeach
-                </select>
+        <div class="form-group">
+            <label for="add_subject">Thêm môn</label>
+            <select id="add_subject" class="form-control">
+                <option value="" disabled selected>Hãy chọn môn</option>
+                @foreach ($subjects as $subject)
+                @if (in_array($subject->id,array_keys($subject_arr)))
+                <option value="{{ $subject->id }}" hidden>{{ $subject->name }}</option>
+                @continue
+                @endif
+                <option value="{{ $subject->id }}">{{ $subject->name }}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="form-group mb-0 justify-content-end row">
+            <div class="col-12">
+                <button type="submit" class="btn btn-info  ">Submit</button>
             </div>
-            <div class="form-group mb-0 justify-content-end row">
-                <div class="col-12">
-                    <button type="submit" class="btn btn-info  ">Submit</button>
-                </div>
-            </div>
-        </form>
+        </div>
+    </form>
 </div>
 @endsection
 @push('js')
