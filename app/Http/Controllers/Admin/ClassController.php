fcc34c373d7cd8baa3c616c8e812f37b8552abfd
HurdyGutty <htnhat1996@gmail.com>
2022-07-16 20:00:47 +0700

    Prepare for Class

---
+++ app/Http/Controllers/Admin/ClassController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\ClassModel;
+use Illuminate\Http\Request;
+
+class ClassController extends Controller
+{
+    public function index()
+    {
+        
+    }
+    public function show(ClassModel $class)
+    {
+
+    }
+}
