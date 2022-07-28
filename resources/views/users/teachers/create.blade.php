656dc20d1fb24297942245c344f9d27f69c9918e
HurdyGutty <htnhat1996@gmail.com>
2022-07-28 20:00:32 +0700

    Add createClass route & blade

---
+++ resources/views/users/teachers/create.blade.php
@@ -0,0 +1,118 @@
+@extends('layout-dashboard-site.master')
+@push('css')
+<link href="{{asset('css/now-ui-kit.css?v=1.1.0')}}" rel="stylesheet" />
+@endpush
+@section('content')
+<div class="card-body">
+    <h2 class="header-title">Thêm ngành</h2>
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+    <form method="POST" action="{{ route('app.user.storeClass') }}" enctype="multipart/form-data">
+        @csrf
+        <div class="form-group">
+            <label for="subject_input" class="col-form-label">Chọn môn</label>
+            <select class="form-control" id="subject_input" name="name">
+                <option disabled>Chọn môn</option>
+                @foreach($subjects as $id=>$name)
+                <option value="{{$id}}">{{$name}}</option>
+                @endforeach
+            </select>
+            @if ($errors->has('name'))
+            <div class="text-danger mt-1">
+                {{$errors->first('name')}}
+            </div>
+            @endif
+        </div>
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <label for="weekday1" class="col-form-label">Chọn thứ</label>
+                <select class="form-control" id="weekday1" name="weekday1">
+                    <option disabled>Chọn thứ</option>
+                    <option value="1">Thứ hai</option>
+                    <option value="2">Thứ ba</option>
+                    <option value="3">Thứ tư</option>
+                    <option value="4">Thứ năm</option>
+                    <option value="5">Thứ sáu</option>
+                    <option value="6">Thứ bảy</option>
+                    <option value="7">Chủ nhật</option>
+                </select>
+                @if ($errors->has('weekday1'))
+                <div class="text-danger mt-1">
+                    {{$errors->first('weekday1')}}
+                </div>
+                @endif
+            </div>
+            <div class="form-group col-md-6">
+                <label for="time1" class="col-form-label">Chọn ca</label>
+                <select class="form-control" id="time1" name="time1">
+                    <option disabled>Chọn ca</option>
+                    <option value="1">17h00 - 19h00</option>
+                    <option value="2">19h00 - 21h00</option>
+                </select>
+                @if ($errors->has('time1'))
+                <div class="text-danger mt-1">
+                    {{$errors->first('time1')}}
+                </div>
+                @endif
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <label for="weekday2" class="col-form-label">Chọn thứ</label>
+                <select class="form-control" id="weekday2" name="weekday2">
+                    <option disabled>Chọn thứ</option>
+                    <option value="1">Thứ hai</option>
+                    <option value="2">Thứ ba</option>
+                    <option value="3">Thứ tư</option>
+                    <option value="4">Thứ năm</option>
+                    <option value="5">Thứ sáu</option>
+                    <option value="6">Thứ bảy</option>
+                    <option value="7">Chủ nhật</option>
+                </select>
+                @if ($errors->has('weekday2'))
+                <div class="text-danger mt-1">
+                    {{$errors->first('weekday2')}}
+                </div>
+                @endif
+            </div>
+            <div class="form-group col-md-6">
+                <label for="time2" class="col-form-label">Chọn ca</label>
+                <select class="form-control" id="time2" name="time2">
+                    <option disabled>Chọn ca</option>
+                    <option value="1">17h00 - 19h00</option>
+                    <option value="2">19h00 - 21h00</option>
+                </select>
+                @if ($errors->has('time2'))
+                <div class="text-danger mt-1">
+                    {{$errors->first('time2')}}
+                </div>
+                @endif
+            </div>
+        </div>
+        <div class="form-group mb-0 justify-content-end row">
+            <div class="col-12">
+                <button type="submit" class="btn btn-info">Đăng ký</button>
+            </div>
+        </div>
+    </form>
+</div>
+@endsection
+@push('js')
+<script src="{{asset('js/plugins/bootstrap-tagsinput.js')}}"></script>
+<script>
+imgInp.onchange = evt => {
+    const [file] = imgInp.files
+    if (file) {
+        blah.src = URL.createObjectURL(file)
+        $('#blah').attr("hidden", false)
+    }
+}
+</script>
+@endpush
