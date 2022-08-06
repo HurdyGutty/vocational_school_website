08d04a5466a54d96e2e64c55917d8235c599157b
HurdyGutty <htnhat1996@gmail.com>
2022-08-07 01:53:10 +0700

    Add  register feature in AuthController

--- app/Http/Controllers/App/AuthController.php
+++ app/Http/Controllers/App/AuthController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\App;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterRequest;
 use App\Http\Requests\LoginRequest;
 use App\Lib\JWT\JWT;
 use App\Models\User;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\DB;
 
 class AuthController extends Controller
 {
@@ -29,26 +31,29 @@ class AuthController extends Controller
         if ($user instanceof User) {
             $token = $this->generateToken($user);
             session()->put('token', $token);
+
             return redirect()->route('app.index');
         }
+
         return redirect()->back();
     }
 
     public function generateToken(User $user)
     {
         $jwt = c(JWT::class);
-        $token = $jwt->encode([
+
+        return $jwt->encode([
             'id' => $user->id,
             'name' => $user->name,
             'role' => $user->role,
-            'is_admin' => false
+            'is_admin' => false,
         ]);
-        return $token;
     }
 
     public function logOut(): RedirectResponse
     {
         session()->forget('token');
+
         return redirect()->route('index');
     }
 
@@ -58,6 +63,50 @@ class AuthController extends Controller
         if ($user instanceof User && $user->verify($password)) {
             return $user;
         }
+
         return null;
+    }
+
+    public function registerUser(RegisterRequest $request)
+    {
+        $name = $request->validated()['name'];
+        $gender = $request->validated()['gender'];
+        $date_of_birth = $request->validated()['date_of_birth'];
+        $phone = $request->validated()['phone'];
+        $email = $request->validated()['email'];
+        $password = $request->validated()['password'];
+        $image = $request->validated()['image'];
+
+        try {
+            DB::Transaction(function () use ($name, $gender, $date_of_birth, $phone, $email, $password, $image) {
+                if (isset($image)) {
+                    $image = saveImage($image)->id;
+                }
+
+                User::create([
+                    'name' => $name,
+                    'gender' => $gender,
+                    'date_of_birth' => $date_of_birth,
+                    'phone' => $phone,
+                    'email' => $email,
+                    'password' => $password,
+                    'role' => 0,
+                ]);
+
+                $user = $this->auth($email, $password);
+                if ($user instanceof User) {
+                    $token = $this->generateToken($user);
+                    session()->put('token', $token);
+
+                    return redirect()->route('app.index');
+                }
+
+                return redirect()->back();
+            });
+        } catch (\Exception $ex) {
+            return redirect()->back()->with([
+                'createError' => 'Không tạo được tài khoản',
+            ]);
+        }
     }
 }
