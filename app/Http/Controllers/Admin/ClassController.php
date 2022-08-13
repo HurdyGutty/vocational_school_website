553ae877ff8b018dff08a1b5d5ee63f97f9f5eca
HurdyGutty <htnhat1996@gmail.com>
2022-08-13 18:47:52 +0700

    Add approveSubscription, deleteSubscrition in ClassController

--- app/Http/Controllers/Admin/ClassController.php
+++ app/Http/Controllers/Admin/ClassController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Class\SubscriptionApproveRequest;
 use App\Models\ClassModel;
 use App\Models\Subscription;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ClassController extends Controller
@@ -41,5 +43,20 @@ class ClassController extends Controller
         return view('classes.subscription.pending', [
             'pending_classes' => $pending_classes
         ]);
+    }
+
+    public function approveSubscription(SubscriptionApproveRequest $request)
+    {
+        $subscription = $request->validated();
+        Subscription::where('student_id', $subscription['student_id'])
+            ->where('class_id', $subscription['class_id'])
+            ->update(['admin_id' => getAccount()->id]);
+        return redirect()->route('admin.class.pendingSubscription');
+    }
+
+    public function deleteSubscrition($class_id, $student_id)
+    {
+        Subscription::where('class_id', $class_id)->where('student_id', $student_id)->delete();
+        return redirect()->route('admin.class.pendingSubscription');
     }
 }
