3ae5a11e01f3d0497c0450d0459bd8a8b9ebce4e
HurdyGutty <htnhat1996@gmail.com>
2022-08-16 23:34:25 +0700

    Update GetTeacherClassService.php

--- app/Services/GetTeacherClassService.php
+++ app/Services/GetTeacherClassService.php
@@ -13,35 +13,34 @@ class GetTeacherClassService implements GetClassesInterface
 
     public function __construct()
     {
-        $this->first_query=ClassModel::where('teacher_id','=', getAccount()->id)
-            ->with(['schedules' =>
-                        fn($query) => $query
-                                    ->select('id','class_id','date','start_time','end_time')
-                                    ->where('date','>', Carbon::today())
-                                    ->orderBy('date')
-    ]);
+        $this->first_query = ClassModel::where('teacher_id', '=', getAccount()->id)
+            ->with([
+                'schedules' =>
+                fn ($query) => $query
+                    ->select('id', 'class_id', 'date', 'start_time', 'end_time')
+                    ->where('date', '>', Carbon::today())
+                    ->orderBy('date')
+            ]);
     }
-    
+
 
     public function getClasses(): Collection
     {
         return $this->first_query->get()
-        ->map(function($class){
-            $class->schedule_date = $class->schedules->pluck('date')->first();
-            $class->start_time = $class->schedules->pluck('start_time')->first();
-            $class->end_time = $class->schedules->pluck('end_time')->first();
-            $class->teacher_name = getAccount()->name;
-            unset($class->schedules);
-            return $class;
-        })
-        ;
+            ->map(function ($class) {
+                $class->schedule_date = $class->schedules->pluck('date')->first();
+                $class->start_time = $class->schedules->pluck('start_time')->first();
+                $class->end_time = $class->schedules->pluck('end_time')->first();
+                $class->teacher_name = getAccount()->name;
+                unset($class->schedules);
+                return $class;
+            });
     }
 
     public function getOneClass(int|ClassModel $class_id): ClassModel
     {
-        $class_id = $class_id->id??$class_id;
-        return $this->first_query->where('id',$class_id)
-        ->first();
+        $class_id = $class_id->id ?? $class_id;
+        return $this->first_query->where('id', $class_id)
+            ->first();
     }
-
 }
