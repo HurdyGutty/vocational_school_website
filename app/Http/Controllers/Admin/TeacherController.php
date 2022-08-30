b5e84192be1eda738f0bc287915ef8c6612c0563
HurdyGutty <htnhat1996@gmail.com>
2022-08-30 16:28:05 +0700

    Create TeacherController.php

---
+++ app/Http/Controllers/Admin/TeacherController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class TeacherController extends Controller
+{
+    public function index()
+    {
+    }
+
+    public function show(User $teacher)
+    {
+    }
+
+
+
+    public function create()
+    {
+    }
+
+    public function store()
+    {
+    }
+
+    public function lock()
+    {
+    }
+
+    public function unlock()
+    {
+    }
+}
