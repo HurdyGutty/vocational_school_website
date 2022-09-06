e2166c8116308640f63ef4ebd089cd5c651820ad
HurdyGutty <htnhat1996@gmail.com>
2022-09-06 19:51:56 +0700

    Update search.blade.php

--- resources/views/explore/components/search.blade.php
+++ resources/views/explore/components/search.blade.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-md-6 ml-auto mr-auto">
                 <div class="text-center">
-                    <h2 class="title">Tìm kiếm môn học của bạn</h2>
+                    <h2 class="title mt-4 mb-1">Tìm kiếm ngành học của bạn</h2>
                 </div>
                 <div class="card card-raised card-form-horizontal">
                     <div class="card-body">
@@ -14,7 +14,8 @@
                                         <span class="input-group-addon">
                                             <i class="now-ui-icons ui-1_zoom-bold"></i>
                                         </span>
-                                        <input type="text" name="search" class="form-control" placeholder="Tìm kiếm...">
+                                        <input type="text" name="major_name" class="form-control"
+                                            value="{{old('major_name')}}" placeholder="Tìm kiếm ngành học...">
                                     </div>
                                 </div>
                                 <div class="col-sm-4">
