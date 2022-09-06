369d1d7ed96df4df2cff787f3400289eee4637a5
HurdyGutty <htnhat1996@gmail.com>
2022-09-06 19:52:09 +0700

    Update classes.blade.php

--- resources/views/explore/components/classes.blade.php
+++ resources/views/explore/components/classes.blade.php
@@ -3,6 +3,9 @@
         <h3>Các lớp học</h3>
     </div>
     <div class="row">
+        @if (empty($classes))
+        <h3>Không tìm thấy lớp</h3>
+        @endif
         @foreach ($classes as $class)
         <div class="col-md-6 col-lg-4">
             <div class="card card-blog card-plain">
@@ -11,13 +14,14 @@
                 </div>
                 <div class="card-body">
                     <h5 class="category text-warning">
-                        <i class="now-ui-icons users_single-02"></i>Giáo viên: {{$class->teacher->name}}
+                        <i class="now-ui-icons users_single-02"></i>
+                        <a href="">Giáo viên: {{$class->teacher_name}}</a>
                     </h5>
                     <h4 class="card-title">
-                        {{$class->name}}
+                        {{$class->classes_name}}
                     </h4>
                     @if (!empty(getAccount()))
-                    <a href="{{route('app.user.registerClass',$class)}}">
+                    <a href="{{route('app.user.registerClass',['class' => $class->class_id])}}">
                         <div class="btn-primary btn-round btn-block text-center">Tham gia</div>
                     </a>
                     @else
