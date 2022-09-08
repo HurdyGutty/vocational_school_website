7247760838580774f4b08d32fad03a3444296662
HurdyGutty <htnhat1996@gmail.com>
2022-09-08 20:42:28 +0700

    Reformat ShowTeacher

--- app/Services/ShowAccountService/ShowTeacher.php
+++ app/Services/ShowAccountService/ShowTeacher.php
@@ -18,9 +18,16 @@ class ShowTeacher
     {
         return User::leftJoinSub(
             ClassModel::where('classes.status', '>', 0)
-                ->select('teacher_id', DB::raw('count(class_id) as class_count'),  DB::raw('sum(student_count) as student_sum'))
+                ->select(
+                    'teacher_id',
+                    DB::raw('count(class_id) as class_count'),
+                    DB::raw('sum(student_count) as student_sum')
+                )
                 ->joinSub(
-                    Subscription::select('class_id', DB::raw('count(student_id) as student_count'))
+                    Subscription::select(
+                        'class_id',
+                        DB::raw('count(student_id) as student_count')
+                    )
                         ->groupBy('class_id'),
                     's',
                     fn ($j) => $j->on('s.class_id', '=', 'classes.id')
@@ -30,7 +37,14 @@ class ShowTeacher
             fn ($j) => $j->on('classes.teacher_id', '=', 'users.id')
         )
             ->where('users.id', $id)
-            ->select('users.id', 'users.name', 'users.gender', DB::raw('YEAR(date_of_birth) as birth_year'), 'class_count', 'student_sum')
+            ->select(
+                'users.id',
+                'users.name',
+                'users.gender',
+                DB::raw('YEAR(date_of_birth) as birth_year'),
+                'class_count',
+                'student_sum'
+            )
             ->first();
     }
 }
