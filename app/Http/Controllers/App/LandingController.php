3e9e56485074454f5a578edbaf2714a0c45b8b44
HurdyGutty <htnhat1996@gmail.com>
2022-09-13 17:44:33 +0700

    Update LandingController.php

--- app/Http/Controllers/App/LandingController.php
+++ app/Http/Controllers/App/LandingController.php
@@ -3,13 +3,24 @@
 namespace App\Http\Controllers\App;
 
 use App\Http\Controllers\Controller;
+use App\Models\ClassModel;
+use App\Models\Subscription;
+use App\Services\CalendarService;
 
 class LandingController extends Controller
 {
     public function index()
     {
-        return view('layout-dashboard-site.master');
+        $classes = (new CalendarService(getAccount()->role, getAccount()->id))->getClasses();
+
+        return view('schedule.index', [
+            'classes' => $classes,
+        ]);
     }
 
-
+    public function calendar(int $class_id)
+    {
+        $calendar = CalendarService::getSchedule($class_id);
+        return response()->json($calendar, 200);
+    }
 }
